d492b6a8e045b4ad35d8791b8194e84f0e36f417
MrAzarel <arseniy.mazurenko.03@gmail.com>
2023-05-11 20:06:30 +0300

    JSON

---
+++ Furniture/Serialize.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Furniture
+{
+    internal class Serialize
+    {
+        StreamWriter sw = new StreamWriter("C:\\Users\\Арсений\\source\\repos\\Furniture-auto-placement-app\\Json.json");
+        string json;
+        IPolygon? restoredPerson;
+        public void polygonSerialize(IPolygon polygon)
+        {
+            json = JsonSerializer.Serialize(polygon);
+            sw.WriteLine(json);
+            sw.Close();
+        }
+
+        public void polygonDeserialize()
+        {
+            restoredPerson = JsonSerializer.Deserialize<IPolygon>(json);
+        }
+    }
+}
